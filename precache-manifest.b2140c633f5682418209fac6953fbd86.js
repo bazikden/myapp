157d0e2df8ec38882325867d177e9c53df421c43
bazikden <bazmail0@gmail.com>
2019-10-31 20:47:41 +0200

    Updates

---
+++ precache-manifest.b2140c633f5682418209fac6953fbd86.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55f56348ccecdd4f646604bfedee64a",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "5cb7061bf5b511838acd",
+    "url": "/myapp/static/css/main.0decf838.chunk.css"
+  },
+  {
+    "revision": "7b52c6f4fd46bcbcca28",
+    "url": "/myapp/static/js/2.75960628.chunk.js"
+  },
+  {
+    "revision": "5cb7061bf5b511838acd",
+    "url": "/myapp/static/js/main.77c75a61.chunk.js"
+  },
+  {
+    "revision": "db339d07efbce82ab20f",
+    "url": "/myapp/static/js/runtime~main.3d5bda68.js"
+  },
+  {
+    "revision": "4591afda4853c5f18d118ea844041bf4",
+    "url": "/myapp/static/media/Beans_logo.4591afda.svg"
+  },
+  {
+    "revision": "120a377caff7231b3ac5acc54139d9b7",
+    "url": "/myapp/static/media/Beans_logo_dark.120a377c.svg"
+  },
+  {
+    "revision": "7413c67d7da6e7c04269d1a4079069b2",
+    "url": "/myapp/static/media/Coffee_bg.7413c67d.jpg"
+  },
+  {
+    "revision": "6e5b2328153725886030e90aa399e5b6",
+    "url": "/myapp/static/media/Logo.6e5b2328.svg"
+  },
+  {
+    "revision": "d2a4807b0aa4a7ca1e7a82ee730e6fb6",
+    "url": "/myapp/static/media/Logo.d2a4807b.png"
+  },
+  {
+    "revision": "8c7fe0916425b04a3760438dc40e4e35",
+    "url": "/myapp/static/media/Logo_black.8c7fe091.svg"
+  },
+  {
+    "revision": "2fdcd636b72b8452c9d51e5635185ddb",
+    "url": "/myapp/static/media/Main_bg.2fdcd636.jpg"
+  },
+  {
+    "revision": "44f413ed360df8f02ad8e919491f2570",
+    "url": "/myapp/static/media/avatar2.44f413ed.png"
+  },
+  {
+    "revision": "a956e5ef75eb6c4c46fb8251f7e8cc5f",
+    "url": "/myapp/static/media/banner-bg-2560x676.a956e5ef.jpg"
+  },
+  {
+    "revision": "1a0fe3e3710b4b959ac49eed96689591",
+    "url": "/myapp/static/media/cellphone.1a0fe3e3.svg"
+  },
+  {
+    "revision": "6e7d1e4eda3c4931aa8e6c76b0b6f465",
+    "url": "/myapp/static/media/coffee_girl.6e7d1e4e.jpg"
+  },
+  {
+    "revision": "bde3201c58c61fbce8d0e1a9f3b58e9c",
+    "url": "/myapp/static/media/coffee_item.bde3201c.jpg"
+  },
+  {
+    "revision": "2963787969ffcb9cc7324947abf540d5",
+    "url": "/myapp/static/media/css.29637879.svg"
+  },
+  {
+    "revision": "20caa7d0c0ff0f6b546395ee0b916408",
+    "url": "/myapp/static/media/facebook.20caa7d0.svg"
+  },
+  {
+    "revision": "887c14b1893ab76ea10a81d7006d35e1",
+    "url": "/myapp/static/media/hamburger-menu-icon-transparent-16.887c14b1.jpg"
+  },
+  {
+    "revision": "d882d855ad0c93a45176c40e0dacf8ce",
+    "url": "/myapp/static/media/html.d882d855.svg"
+  },
+  {
+    "revision": "f263dbf1758175dcc7fb5e925373cd68",
+    "url": "/myapp/static/media/instagram.f263dbf1.svg"
+  },
+  {
+    "revision": "cf53a810c6682334aa9a2e46a4b86ce4",
+    "url": "/myapp/static/media/javascript.cf53a810.svg"
+  },
+  {
+    "revision": "3b500131c4e9086a78a4490f6abc3f07",
+    "url": "/myapp/static/media/line-menu.3b500131.svg"
+  },
+  {
+    "revision": "569eb74ac7a0deb9585e7fc04f9cde0c",
+    "url": "/myapp/static/media/linkedin.569eb74a.svg"
+  },
+  {
+    "revision": "10919850f9b0d50925a13d7db5d3eb21",
+    "url": "/myapp/static/media/logo.10919850.svg"
+  },
+  {
+    "revision": "ae7e47ddc289dd05a4c7a852a0ae6e06",
+    "url": "/myapp/static/media/mail.ae7e47dd.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/myapp/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "2afa77717943906cc812fb6a7a97aeee",
+    "url": "/myapp/static/media/man-mobile.2afa7771.svg"
+  },
+  {
+    "revision": "b69bd070042bf27627c518c04add96f2",
+    "url": "/myapp/static/media/paper.b69bd070.jpg"
+  },
+  {
+    "revision": "ca2f60a309344b3d4e767bc00e31b45e",
+    "url": "/myapp/static/media/phone.ca2f60a3.svg"
+  },
+  {
+    "revision": "603e7eb001182ac09947fe8e50c3bb96",
+    "url": "/myapp/static/media/pinterest.603e7eb0.svg"
+  },
+  {
+    "revision": "a9a773c48e0c7cece9d6e1cf49abad7e",
+    "url": "/myapp/static/media/sign-out.a9a773c4.svg"
+  },
+  {
+    "revision": "1129b934627f3c8747512c1baf31d7f1",
+    "url": "/myapp/static/media/third-block-bg-2560x692.1129b934.jpg"
+  },
+  {
+    "revision": "3e993ec30ed135283d2b632b79c009fa",
+    "url": "/myapp/static/media/twitter.3e993ec3.svg"
+  },
+  {
+    "revision": "0592ecbecaf9e23b1f6904c916341512",
+    "url": "/myapp/static/media/user-superstar-2x.0592ecbe.jpg"
+  }
+]);
